fc9133d28ec8d497f83e53e6bacb6603b432fbe6
AidaG91 <aida.garmus@gmail.com>
2026-04-24 16:09:10 +0200

    feat: setup project structure and products data export/import

---
+++ src/js/data.js
@@ -0,0 +1,19 @@
+export const products = [
+  {
+    id: 1,
+    image: "../assets/images/classic-yellow.png",
+    name: "Classic Yellow Duck",
+    price: 9.99,
+    description:
+      'The icon of childhood joy. reimagined for the modern collector. Our signature yellow duck features a perfectly weighted bottom for the ultimate floating experience. Hand-finished for that premium tactile "squish" that defines a DuckStore original.',
+    stock: 42,
+    category: "Classic",
+    badge1Img: "../assets/icons/Unsincable.svg",
+    badge1: "Unsinkable",
+    badge1Class: "product-badges",
+    badge2Img: "../assets/icons/Non-Toxic.svg",
+    badge2: "Non-Toxic",
+    badge2Class: "product-badges",
+  },
+  { id: 2, name: "Pato Batman", price: 20, category: "Cine" },
+];
